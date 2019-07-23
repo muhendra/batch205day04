1d692c7d797e45135b12dd9ed96169c0f82734b9
vidyaamelia <vidyamelia@gmail.com>
2019-07-23 14:46:00 +0700

    first commit

---
+++ sLatihanDay04/caDay04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay04
+{
+    class Program
+    {
+        public static int hitung(int x)
+        {
+            return x + 2;
+        }
+        static void Main(string[] args)
+        {
+            int angka = 3;
+            bool status = true;
+
+            Console.Write("ABC {0} {0}\n" , angka, status);
+            Console.Write("ABC" + angka + " " + status);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sLatihanDay04/caSimpleArraySum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caSimpleArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(int[] ar)
+        {
+            /*
+             * Write your code here.
+             */
+
+            int hasil = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                hasil = ar[i] + hasil;
+            }
+
+            return hasil;
+
+        }
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = simpleArraySum(ar);
+
+            Console.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
+
